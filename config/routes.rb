daba17200a82a4b378ea71895ea431e61e762b9c
Henry Doan <henrydoan96@gmail.com>
2019-01-12 21:17:16 -0700

    added brain back end

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :items
+    get '/braintree_token', to: 'braintree#token'
+    post '/payment', to: 'braintree#payment'
   end
 end
